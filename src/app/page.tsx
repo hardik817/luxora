5e6f22d8b5d830c3dd1aa4484b0a255b1cd5b238
HARDIK SACHDEVA <sachdevahardik22@gmail.com>
2025-01-03 16:13:24 +0530

    slider complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+import Productlist from "@/components/Productlist"
+import Slider from "@/components/slider"
+
 const HomePage = () => {
   return (
-    <div className=''>HomePage</div>
+    <>
+      <div className="">
+        <Slider />
+        <div className="mt-24 px-4 md:px-8 lg:px-32 xl:px-8 2xl:px-36">
+          <h1>Featured Products</h1>
+          <Productlist />
+        </div>
+      </div>
+    </>
+
   )
 }
 
---
+++ src/components/Productlist.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+import Image from "next/image"
+const Productlist = () => {
+    return (
+        <div className='mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap'>
+            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+                <div className="relative w-full h-80">
+                    <Image alt="" src="https://images.pexels.com/photos/6117187/pexels-photo-6117187.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                        fill
+                        sizes="25vw"
+                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+                    ></Image>
+                    <Image alt="" src="https://images.pexels.com/photos/28052361/pexels-photo-28052361/free-photo-of-a-wooden-table-with-bowls-and-cups-on-it.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                        fill
+                        sizes="25vw"
+                        className="absolute object-cover rounded-md"
+                    ></Image>
+                </div>
+                <div className="flex justify-between">
+                    <span className="font-medium">Product Name</span>
+                    <span className="font-semibold">$49</span>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default Productlist;
