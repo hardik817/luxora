5c713fcf2af4888d0236a1183dcf24e7237a175a
HARDIK SACHDEVA <sachdevahardik22@gmail.com>
2025-01-08 22:37:33 +0530

    product Image page partially completed

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,14 @@
+import ProductImages from "@/components/productImages"
 const SinglePage = () => {
     return (
-        <div className=''>SinglePage</div>
+        <div className='px-4 md:px-8 lg:px-32 xl:px-8 2xl:px-36 relative flex flex-col lg:flex-row gap-16'>
+            {/*Image */}
+            <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+                <ProductImages />
+            </div>
+            {/*Texts */}
+            <div className="w-full lg:w-1/2 flex flex-col gap-6">Texts</div>
+        </div>
     )
 }
 
