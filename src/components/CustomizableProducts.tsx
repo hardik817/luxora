b0c095b166c363423bfa5dc6f435a0bc2bf0118c
HARDIK SACHDEVA <sachdevahardik22@gmail.com>
2025-01-11 21:19:21 +0530

    Update CustomizableProducts.tsx

--- src/components/CustomizableProducts.tsx
+++ src/components/CustomizableProducts.tsx
@@ -1,8 +1,12 @@
 const CustomizableProducts = () => {
     return (
         <div className="flex flex-col gap-6">
-            <h4>Choose a color</h4>
-            <div></div>
+            <h4 className="font-medium">Choose a color</h4>
+            <div className="flex items-center gap-3">
+
+            </div>
+            <h4 className="font-medium">Choose a size</h4>
+            <div className="flex items-center gap-3"></div>
         </div>
     )
 }
