144d920bbfd881a518ee024fee02c2730d7d5ac0
HARDIK SACHDEVA <sachdevahardik22@gmail.com>
2025-03-02 14:13:57 +0530

    commits

--- src/components/cartModel.tsx
+++ src/components/cartModel.tsx
@@ -105,7 +105,7 @@ const CartModel = () => {
                     <div className="">
                         <div className="flex items-center justify-between font-semibold">
                             <span className="">Subtotal</span>
-                            <span className="">${cart.subtotal.amount}</span>
+                            <span className="">${cart.currency}</span>
                         </div>
                         <p className="text-gray-500 text-sm mt-2 mb-4">
                             Shipping and taxes calculated at checkout.
